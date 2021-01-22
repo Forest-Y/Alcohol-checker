e0dd05c760334b5c2b6bc85a4121bd413f128817
Mori <yuuya15009@gmail.com>
2021-01-22 16:15:42 +0900

    commit

--- src/components/SerchAlert.jsx
+++ src/components/SerchAlert.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { IonAlert　} from '@ionic/react';
+import React, { useEffect, useState } from 'react';
+import { IonAlert } from '@ionic/react';
 //import { useSpeechRecognition } from "react-speech-recognition"
 import "react-dom"
 import searchData from './searchData';
@@ -9,25 +9,39 @@ const SerchAlert = (props) => {
     //const { transcript, resetTranscript } = useSpeechRecognition()
     const allData = JSON.parse(localStorage.alcohol)
     const { transcript } = useSpeechRecognition()
+    const [text, setText] = useState("")
+    useEffect(() => {
+        SpeechRecognition.startListening()
+    }, [])
+    localStorage.searchWord = transcript
+    if(localStorage.searchWord !== text){
+        setText(localStorage.searchWord)
+    }
+    console.log(localStorage.searchWord)
     return (
-        <IonAlert
-            isOpen={props.searchAlert}
-            onDidDismiss={() => props.setSearchAlert(false)}
-            header="検索"
-            inputs={[{
-                name: "text",
-                type: "text"
-            }]}
-            buttons={[{
-                text: "検索",
-                handler: (text) => {
-                    localStorage.searchWord = text.text
-                    searchData(allData, props.setData)
-                    props.setSearchAlert(false)
+        <div>
+            <IonAlert
+                isOpen={props.searchAlert}
+                onDidDismiss={() => props.setSearchAlert(false)}
+                header="検索"
+                inputs={[{
+                    name: "text",
+                    type: "text",
+                    value: transcript
+                }]}
+                buttons={[{
+                    text: "検索",
+                    handler: () => {
+                        console.log(transcript)
+                        searchData(allData, props.setData)
+                        SpeechRecognition.stopListening()
+                        props.setSearchAlert(false)
+                    }
                 }
-            }
-        ]}
-        />
+                ]}
+            />
+            <p>{transcript}</p>
+        </div>
     );
 };
 
--- src/pages/Tab2.jsx
+++ src/pages/Tab2.jsx
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
-import { IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonButtons, IonInput, IonItem} from '@ionic/react';
+import { IonButton, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonButtons, IonInput, IonItem } from '@ionic/react';
 import ActionSheet from "../components/ActionSheet"
 import './Tab2.css';
 import "react-dom"
 import ActionAlert from '../components/ActionAlert';
-import { add, search } from 'ionicons/icons';
+import { add, mic, search } from 'ionicons/icons';
 import searchData from '../components/searchData';
+import SearchAlert from "../components/SerchAlert"
 
 const Tab2 = (props) => {
   const [showActionSheet, setShowActionSheet] = useState(false)
+  const [searchAlert, setSearchAlert] = useState(false)
   const [actionAlert, setActionAlert] = useState(false)
   const [text, setText] = useState("")
   const allData = JSON.parse(localStorage.alcohol)
+  const [data, setData] = useState(JSON.parse(localStorage.alcohol))
   const deleteButton = (data) => {
     localStorage.key = data[0]
     setShowActionSheet(true)
@@ -29,10 +32,32 @@ const Tab2 = (props) => {
   }
   const reset = () => {
     setText("")
+    localStorage.searchWord = ""
     setData(JSON.parse(localStorage.alcohol))
   }
+  /*
+  function regist() {
+    SpeechRecognition.startListening()
+    console.log("OK")
+    return new Promise(() => {
+      SpeechRecognition.startListening()
+    })
+  }
+  async function speech() {
+    await SpeechRecognition.startListening()
+    localStorage.searchWord = transcript
+    searchData(allData, setData)
+    setText(transcript)
+    console.log("OK!")
+    console.log(transcript)
+  }
+  */
+  /*
+  localStorage.searchWord = transcript
+  searchData(allData, setData)
+  setText(transcript)
+  */
   //const data = JSON.parse(localStorage.alcohol)
-  const [data, setData] = useState(JSON.parse(localStorage.alcohol))
   return (
     <IonPage>
       <IonHeader>
@@ -42,7 +67,7 @@ const Tab2 = (props) => {
             <IonButton slot="end" onClick={() => reset()}>リセット</IonButton>
           </IonButtons>
           <IonButtons slot="start">
-            <IonButton slot="start" onClick={() => props.setRegistModal(false)}>戻る</IonButton>
+            <IonButton onClick={() => props.setRegistModal(false)}>戻る</IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
@@ -57,9 +82,15 @@ const Tab2 = (props) => {
                       slot="icon-only"
                       icon={search} />
                   </IonButton>
-
                 </IonButtons>
                 <IonInput type="text" value={text} placeholder="検索する酒を入力" onIonChange={handleChange}></IonInput>
+                <IonButtons>
+                  <IonButton onClick={() => setSearchAlert(true)} slot="end">
+                    <IonIcon
+                      slot="icon-only"
+                      icon={mic} />
+                  </IonButton>
+                </IonButtons>
               </IonItem>
             </form>
             {Object.entries(data).map((data) => {
@@ -81,6 +112,9 @@ const Tab2 = (props) => {
             <IonIcon icon={add} />
           </IonFabButton>
         </IonFab>
+        {searchAlert === true &&
+          <SearchAlert searchAlert = {searchAlert} setSearchAlert = {setSearchAlert} setData = {setData} />
+        }
         <ActionSheet data={data} setShowActionSheet={setShowActionSheet} showActionSheet={showActionSheet} setData={setData} setRegistModal={props.setRegistModal} />
         <ActionAlert data={data} setActionAlert={setActionAlert} actionAlert={actionAlert} setData={setData} />
       </IonContent>
